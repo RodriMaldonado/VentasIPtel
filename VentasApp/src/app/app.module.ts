cf8c085fdcfe5fb91e8c1c7ccc9858979991fa06
Rodrigo Maldonado <rodrimaldo@hotmail.es>
2023-01-19 12:34:20 -0300

    angular app creada

---
+++ VentasApp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './pages/form/formulario/formulario.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
